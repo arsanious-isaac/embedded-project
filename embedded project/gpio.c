241fb83aa0298b0eb1e6af8ede8ef8cc9c468d91
arsanious isaac <65775188+arsanious-isaac@users.noreply.github.com>
2022-05-20 16:20:46 +0200

    Update gpio.c

--- embedded project/gpio.c
+++ embedded project/gpio.c
@@ -1,47 +1,39 @@
 #include "gpio.h"
 
 void EdgeCounter_Init(void){                          
-  SYSCTL_RCGC2_R |= 0x00000020; // (a) activate clock for port F
-  GPIO_PORTF_DIR_R &= ~0x10;    // (c) make PF4 in (built-in button)
-  GPIO_PORTF_AFSEL_R &= ~0x10;  //     disable alt funct on PF4
-  GPIO_PORTF_DEN_R |= 0x10;     //     enable digital I/O on PF4   
-  GPIO_PORTF_PCTL_R &= ~0x000F0000; // configure PF4 as GPIO
-  GPIO_PORTF_AMSEL_R = 0;       //     disable analog functionality on PF4
-  GPIO_PORTF_PUR_R |= 0x10;     //     enable weak pull-up on PF4
-  GPIO_PORTF_IS_R &= ~0x10;     // (d) PF4 is edge-sensitive
-  GPIO_PORTF_IBE_R &= ~0x10;    //     PF4 is not both edges
-  GPIO_PORTF_IEV_R &= ~0x10;    //     PF4 falling edge event
-  GPIO_PORTF_ICR_R = 0x10;      // (e) clear flag4
-  GPIO_PORTF_IM_R |= 0x10;      // (f) arm interrupt on PF4
+  SYSCTL_RCGC2_R |= 1<<4;  // (a) activate clock for port E
+  GPIO_PORTE_DIR_R &= ~0x10;    // (c) make PF4 in (built-in button)
+  GPIO_PORTE_AFSEL_R &= ~0x10;  //     disable alt funct on PF4
+  GPIO_PORTE_DEN_R |= 0x10;     //     enable digital I/O on PF4   
+  GPIO_PORTE_PCTL_R &= ~0x000F0000; // configure PF4 as GPIO
+  GPIO_PORTE_AMSEL_R = 0;       //     disable analog functionality on PF4
+  GPIO_PORTE_PUR_R |= 0x10;     //     enable weak pull-up on PF4
+  GPIO_PORTE_IS_R &= ~0x10;     // (d) PF4 is edge-sensitive
+  GPIO_PORTE_IBE_R &= ~0x10;    //     PF4 is not both edges
+  GPIO_PORTE_IEV_R &= ~0x10;    //     PF4 falling edge event
+  GPIO_PORTE_ICR_R = 0x10;      // (e) clear flag4
+  GPIO_PORTE_IM_R |= 0x10;      // (f) arm interrupt on PF4
 	
-	GPIO_PORTF_DIR_R &= ~0x01;    // (c) make PF0 in (built-in button)
-  GPIO_PORTF_AFSEL_R &= ~0x01;  //     disable alt funct on PF0
-  GPIO_PORTF_DEN_R |= 0x01;     //     enable digital I/O on PF0   
-  GPIO_PORTF_PCTL_R &= ~0x000000F0; // configure PF0 as GPIO
-  GPIO_PORTF_AMSEL_R = 0;       //     disable analog functionality on PF0
-  GPIO_PORTF_PUR_R |= 0x01;     //     enable weak pull-up on PF0
-  GPIO_PORTF_IS_R &= ~0x01;     // (d) PF0 is edge-sensitive
-  GPIO_PORTF_IBE_R &= ~0x01;    //     PF0 is not both edges
-  GPIO_PORTF_IEV_R &= ~0x01;    //     PF0 falling edge event
-  GPIO_PORTF_ICR_R = 0x01;      // (e) clear flag0
-  GPIO_PORTF_IM_R |= 0x01;      // (f) arm interrupt on PF0
-
   NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
   NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC
   EnableInterrupts();           // (i) Clears the I bit
 }
 
-void PortF_Init(void){ volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
+void PortE_Init(void){ 
+	volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x00000010;     // 1) F clock
   delay = SYSCTL_RCGC2_R;           // delay   
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
-  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0       
-  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
-  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
-  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
-  GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
-  GPIO_PORTF_PUR_R = 0x11;          // enable pullup resistors on PF4,PF0       
-  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0         
+  GPIO_PORTE_LOCK_R = 0x4C4F434B;   // 2) unlock PortE
+  GPIO_PORTE_CR_R = 0x1E;           // allow changes to PE4-0     
+  GPIO_PORTE_AMSEL_R = 0x00;        // 3) disable analog function
+  GPIO_PORTE_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
+  GPIO_PORTE_DIR_R = 0x02;          // 5) PE0 input, PE1 output   
+  GPIO_PORTE_AFSEL_R = 0x00;        // 6) no alternate function
+  GPIO_PORTE_PDR_R= 0x01;          // enable pulldown resistors on PE0       
+  GPIO_PORTE_DEN_R = 0xFF;  
+	GPIO_PORTE_PUR_R = 0x00;          // enable pullup resistors on PE1,PE0       
+
 }
+
 
 
